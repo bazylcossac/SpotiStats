e238f29c7d47ece36d04c53db5263f5aad4782d7
Michał <102479081+bazylcossac@users.noreply.github.com>
2024-12-08 23:24:12 +0100

    Update apiKeys.ts

--- SpotiStats/src/apiKeys.ts
+++ SpotiStats/src/apiKeys.ts
@@ -1,2 +1,2 @@
-export const clientId = "06d408ab38794edb91b879d117ab204f";
-export const clientSecret = "352f8d5b6fe34be7affa5677e1d8f1f6";
+export const clientId = clientId
+export const clientSecret = clientSecret
